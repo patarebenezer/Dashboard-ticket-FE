f7922418cc900ab7069c63c29ae1b02d85a82afb
patarebenezer <patar@kulego.id>
2024-05-24 13:42:11 +0700

    fix(card): update background color

--- src/components/Dashboard/Overview/Card.js
+++ src/components/Dashboard/Overview/Card.js
@@ -1,6 +1,6 @@
 export default function Card({ title, totalCount }) {
  return (
-  <div className='border-2 border-gray-300 rounded-md w-full py-6 text-center hover:border-blue-600 hover:border-2 hover:cursor-pointer transition duration-300 group'>
+  <div className='border-2 bg-white dark:bg-slate-700 border-gray-300 rounded-md w-full py-6 text-center hover:border-blue-600 hover:border-2 hover:cursor-pointer transition duration-300 group'>
    <p className='text-gray-400 font-semibold text-lg pb-2 group-hover:text-blue-600'>
     {title}
    </p>
